7ea03414332d691e74e9da1b9da24c38388c5d75
Kukel Attila <kukel.attila@arteries.hu>
2023-02-15 17:15:56 +0100

    feat: tömb, foreach listázás

--- index.php
+++ index.php
@@ -3,27 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>02</title>
 </head>
-<body>
-<h1>
+<body style="font-size: 26px">
+<?php
+
+$books = [
+    [
+        'name' => 'Hyperion',
+        'author' => 'Dan Simmons',
+        'year' => 1989,
+    ],
+    [
+        'name' => 'Az utolsó kívánság',
+        'author' => 'Andrzej Sapkowski',
+        'year' => 1993,
+    ],
+    [
+        'name' => 'Alapítvány',
+        'author' => 'Isaac Asimov',
+        'year' => 1951,
+    ],
+];
+
+?>
+<ul>
     <?php
-
-    $name = [
-        'foobar' => 1,
-        2,
-        3,
-        4,
-        5,
-    ];
-
-    echo '..::DEBUGGING::..';
-    echo '<pre>';
-    var_dump($name);
-    echo '</pre>';
-    exit;
-
+    foreach ($books as $book) {
+        ?>
+        <li>
+            <?php
+            echo $book['name'].' ('.$book['year'].')';
+            ?>
+        </li>
+        <?php
+    }
     ?>
-</h1>
+</ul>
 </body>
 </html>
