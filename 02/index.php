861a16fdd2972c87847cac9bf5377b9e1c64ba25
Kukel Attila <kukel.attila@arteries.hu>
2023-02-22 12:36:32 +0100

    feat: logika/view szétválasztása

--- 02/index.php
+++ 02/index.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>02</title>
-</head>
-<body style="font-size: 26px">
 <?php
-
 $books = [
     [
         'name' => 'Hyperion',
@@ -26,20 +16,6 @@ $books = [
         'year' => 1951,
     ],
 ];
+$filteredData = array_filter($books, fn (array $book): bool => $book['year'] < 1990);
 
-$filteredData = array_filter($books, fn (array $book): bool => $book['year'] < 1990)
-
-?>
-<ul>
-    <?php
-    foreach ($filteredData as $book) {
-        ?>
-        <li>
-            <?= $book['name'].' ('.$book['year'].')' ?>
-        </li>
-        <?php
-    }
-    ?>
-</ul>
-</body>
-</html>
+require('index.view.php');
