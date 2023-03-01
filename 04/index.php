1d69dfbd21a2936a20da9335de5d9dd76272132a
Kukel Attila <kukel.attila@arteries.hu>
2023-03-01 13:30:45 +0100

    feat: adatbázis class bevezetése

--- 04/index.php
+++ 04/index.php
@@ -1,18 +1,31 @@
 <?php
 
 require 'functions.php';
+
 //require 'routing.php';
 
-$dsn = 'mysql:host=mysql-local;port=3306;dbname=sze;charset=utf8mb4';
-$pdo = new PDO($dsn, 'root', 'a');
+class Database
+{
+    public function query(string $param)
+    {
+        $dsn = 'mysql:host=mysql-local;port=3306;dbname=sze;charset=utf8mb4';
+        $pdo = new PDO($dsn, 'root', 'a');
 
-$statement = $pdo->prepare('select * from posts');
-$statement->execute();
+        $statement = $pdo->prepare($param);
+        $statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $statement;
+    }
+}
 
-//foreach($posts as $post) {
-//    echo '<li>'.$post['title'].'</li>';
-//}
+$db = new Database();
+$posts = $db
+    ->query('select * from posts')
+    ->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
+foreach ($posts as $post) {
+    echo '<li>'.$post['title'].'</li>';
+}
+
+//dd($posts);
