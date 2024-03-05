3e2d26fca7b7a57756552cbf27f6e97f84ae45d3
Kukel Attila <kukel.attila@arteries.hu>
2024-03-05 11:06:49 +0100

    feat: navbar hozzáadás

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+@include('layouts.partials.navbar')
 <div class="container">
     <div class="p-3">
         @yield('content')
