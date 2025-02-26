675a2da479268c02b0180ff1e6af2add98c00201
Kukel Attila <kukel.attila@gmail.com>
2025-02-26 10:18:29 +0100

    feat(2025): album migration, factory, seeder

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AlbumFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title'  => $this->faker->sentence(3),
+            'artist' => $this->faker->name(),
+            'year'   => $this->faker->year(),
+            'label'  => $this->faker->company(),
+            'duration' => $this->faker->randomDigit(),
+        ];
+    }
+}
