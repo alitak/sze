c21fc15ccbf34d1138ba0c2c9f41cf572ba988cd
Kukel Attila <kukel.attila@gmail.com>
2025-05-22 08:31:54 +0200

    Update setup_exam.bat

--- setup_exam.bat
+++ setup_exam.bat
@@ -3,30 +3,36 @@ setlocal
 
 REM Állítsd be a cél mappa nevét
 set TARGET_DIR=C:\Data\PHP_d104\php-sze-vizsga
+set ZIP_NAME=l11.zip
+set ZIP_PATH=%TARGET_DIR%\%ZIP_NAME%
 
 REM 1. Mappa létrehozása / ürítése
-echo [1/5] Mappa előkészítése: %TARGET_DIR%
+echo [1/6] Mappa előkészítése: %TARGET_DIR%
 if exist "%TARGET_DIR%" (
     echo Mappa létezik, tartalom törlése...
     rmdir /S /Q "%TARGET_DIR%"
 )
 mkdir "%TARGET_DIR%"
 
-REM 2. ZIP letöltése
-echo [2/5] ZIP letöltése...
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/alitak/sze/raw/2025/l11.zip' -OutFile '%TEMP%\l11.zip'"
+REM 2. ZIP letöltése a projekt mappába
+echo [2/6] ZIP letöltése: %ZIP_PATH%
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/alitak/sze/raw/2025/l11.zip' -OutFile '%ZIP_PATH%'"
 
 REM 3. ZIP kicsomagolása
-echo [3/5] ZIP kicsomagolása...
-powershell -Command "Expand-Archive -Path '%TEMP%\l11.zip' -DestinationPath '%TARGET_DIR%' -Force"
+echo [3/6] ZIP kicsomagolása...
+powershell -Command "Expand-Archive -Path '%ZIP_PATH%' -DestinationPath '%TARGET_DIR%' -Force"
 
-REM 4. Docker konténer indítása
-echo [4/5] Docker compose indítása...
+REM 4. ZIP fájl törlése
+echo [4/6] ZIP fájl törlése...
+del "%ZIP_PATH%"
+
+REM 5. Docker konténer indítása
+echo [5/6] Docker compose indítása...
 cd /D "%TARGET_DIR%\.docker"
 docker compose up -d
 
-REM 5. Composer install a konténeren belül
-echo [5/5] Composer install a konténeren...
+REM 6. Composer install a konténeren belül
+echo [6/6] Composer install a konténeren...
 docker exec php-sze composer install
 
 echo ✅ Kész vagyunk!
