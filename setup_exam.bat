b9cbd1ef70ef01b4f10cf375a72ca5c3ccfffde3
Kukel Attila <kukel.attila@gmail.com>
2025-05-22 08:57:11 +0200

    Update setup_exam.bat

--- setup_exam.bat
+++ setup_exam.bat
@@ -6,39 +6,52 @@ set ZIP_NAME=l11.zip
 set ZIP_PATH=%TARGET_DIR%\%ZIP_NAME%
 set INNER_DIR=%TARGET_DIR%\l11
 
-REM 1. Mappa létrehozása / ürítése
-echo [1/7] Mappa előkészítése: %TARGET_DIR%
+REM 1. Docker Desktop indítása (ha nem fut)
+echo [1/7] Docker Desktop indítása...
+tasklist /FI "IMAGENAME eq Docker Desktop.exe" | find /I "Docker Desktop.exe" >nul
+if errorlevel 1 (
+    start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    echo Várunk 15 másodpercet a Docker elindulására...
+    timeout /T 15 >nul
+)
+
+REM 2. Fájlok előkészítése
+echo [2/7] Projektmappa előkészítése...
 if exist "%TARGET_DIR%" (
-    echo Mappa létezik, tartalom törlése...
+    echo Mappa létezik, törlés...
     rmdir /S /Q "%TARGET_DIR%"
 )
 mkdir "%TARGET_DIR%"
 
-REM 2. ZIP letöltése a projekt mappába
-echo [2/7] ZIP letöltése: %ZIP_PATH%
+echo ZIP letöltése...
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/alitak/sze/raw/2025/l11.zip' -OutFile '%ZIP_PATH%'"
 
-REM 3. ZIP kicsomagolása
-echo [3/7] ZIP kicsomagolása...
+echo ZIP kicsomagolása...
 powershell -Command "Expand-Archive -Path '%ZIP_PATH%' -DestinationPath '%TARGET_DIR%' -Force"
 
-REM 4. Tartalom áthelyezése a l11 mappából a gyökérbe
-echo [4/7] Tartalom áthelyezése...
+echo Tartalom áthelyezése...
 xcopy "%INNER_DIR%\*" "%TARGET_DIR%\" /E /H /Y
 rmdir /S /Q "%INNER_DIR%"
 
-REM 5. ZIP fájl törlése
-echo [5/7] ZIP fájl törlése...
+echo ZIP törlése...
 del "%ZIP_PATH%"
 
-REM 6. Docker konténer indítása
-echo [6/7] Docker compose indítása...
+REM 3. Docker Compose indítása
+echo [3/7] Docker konténerek indítása...
 cd /D "%TARGET_DIR%\.docker"
 docker compose up -d
 
-REM 7. Composer install a konténeren belül
-echo [7/7] Composer install a konténeren...
+REM 4. Composer install a konténerben
+echo [4/7] Composer install futtatása...
 docker exec php-sze composer install
 
-echo ✅ Vizsgakörnyezet készen áll!
+REM 5. Chrome megnyitása 2 füllel
+echo [5/7] Chrome böngésző indítása...
+start "" "chrome.exe" "http://localhost:29083" "http://localhost:29082"
+
+REM 6. PhpStorm indítása
+echo [6/7] PhpStorm indítása...
+start "" "C:\Program Files\JetBrains\PhpStorm 2023.3.5\bin\phpstorm64.exe" "%TARGET_DIR%"
+
+echo [7/7] ✅ Vizsgakörnyezet teljesen kész!
 pause
